a69b79dfd637d5433ad7bde5339337e67965259a
Austin Kim <austinyearlykim@gmail.com>
2016-08-28 20:02:56 -0700

    adds mocha testing

---
+++ server/test/unit/thirdPartyApis.js
@@ -0,0 +1,15 @@
+describe('Image API', () => {
+    before((done) => {
+        console.log('before');
+        done();
+    });
+
+    after((done) => {
+        console.log('after');
+        done();
+    });
+
+    it('Able to receive card image URL', (done) => {
+        done();
+    });
+});
