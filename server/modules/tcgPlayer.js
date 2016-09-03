a008bc8e3aa8a4831ca360643d47ca8176a96d5f
Austin Kim <austinyearlykim@gmail.com>
2016-09-02 17:31:10 -0700

    adds async library and redis caching completed

--- server/modules/tcgPlayer.js
+++ server/modules/tcgPlayer.js
@@ -1,37 +1,77 @@
 const tcgPlayer = module.exports;
+const async = require('async');
 const request = require('superagent');
 const redis = require('../index.js');
+const ENV = require('../../.config.js');
 
 tcgPlayer.priceCheck = (req, resp) => {
-    redis.client.get('fooz', function(err, buffer) {
-        console.log('err: ', err);
-        console.log('buffer: ', buffer);
+    var cardSet = req.body.set;
+    var cardName = req.body.name;
+    var redisQuery = cardName + ':' + cardSet;
+    var imageQueryString =  'http://magictcgprices.appspot.com/api/images/imageurl.json?cardname=' + cardName;
+    // var priceQueryString = NOT_APPROVED;
+    // var link = 'http://store.tcgplayer.com/Products.aspx?GameName=Magic&amp;Name=' + cardName +'&partner=' + ENV.TCG_PARTNERKEY;
+    var storage = {
+        low: '0.0240',
+        med: '0.2456',
+        high: '0.4641',
+        link: 'www.tcgplayer.com'
+    };
+
+/*
+    async.parallel({
+        imageUrl: (callback) => {
+            request
+                .get(imageQueryString)
+                .end((err ,res) => {
+                    callback(err, res.body);
+                });
+        },
+        prices: (callback) => {
+            request
+                .get(priceQueryString)
+                .end((err, res) => {
+                    // query and parse xml and turn to json to store in storage
+                    redis.client.set(redisQuery, 'low*med*high');
+                    callback(err, parsedXMLobj);
+                });
+        }
+    }, (err, results) => {
+        resp.json(storage);
     });
-    // var cardName = req.body.name;
-    // var cardSet = req.body.set;
-    // var imageQueryString =  'http://magictcgprices.appspot.com/api/images/imageurl.json?cardname=' + cardName;
-    // var MOCKTCG = {
-    //     low: '0.0240',
-    //     med: '0.2456',
-    //     high: '0.4641',
-    //     link: 'www.tcgplayer.com'
-    // };
-    // request
-    //     .get(imageQueryString)
-    //     .end((err ,res) => {
-    //         if(err) {
-    //             throw err;
-    //         }
-    //         console.log('image query success!');
-    //         MOCKTCG.imgUrl = res.body;
-    //         request
-    //             .get(priceQueryString)
-    //             .end((err, res) => {
-    //                 if(err) {
-    //                     throw err;
-    //                 }
-    //                 console.log('price query success!');
-    //                 resp.json(MOCKTCG);
-    //             });
-    //     });
+*/
+
+    redis.client.get(redisQuery, (err, data) => {
+        if(err !== null) {
+            throw(err);
+        }
+        if(data) {
+            var prices = data;
+            // parse the prices string .split('*');
+            console.log('accessing cache');
+            request
+                .get(imageQueryString)
+                .end((err ,res) => {
+                    if(err) {
+                        throw err;
+                    }
+                    console.log('image query success!');
+                    storage.imgUrl = res.body;
+                    resp.json(storage);
+                });
+        } else {
+            console.log('sike');
+            request
+                .get(imageQueryString)
+                .end((err ,res) => {
+                    if(err) {
+                        throw err;
+                    }
+                    console.log('image query success!');
+                    storage.imgUrl = res.body;
+                    redis.client.set(redisQuery, 'low*med*high');
+                    resp.json(storage);
+                });
+        }
+    });
 };
